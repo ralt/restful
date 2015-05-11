7cac8b1972b6e6fba88265d1f5ae74fe330c4e74
Florian Margaine <florian@margaine.com>
2015-05-11 22:12:51 +0200

    Implements HTTP methods according to rfc7231

--- src/acceptor.lisp
+++ src/acceptor.lisp
@@ -6,8 +6,11 @@
              :type hash-table)))
 
 (defmethod h:acceptor-dispatch-request ((acceptor acceptor) request)
-  (let ((path-parts (mapcar #'string-downcase (rest (cl-ppcre:split "/" (hunchentoot:request-uri request))))))
-    (handle-uri path-parts (slot-value acceptor 'resource))))
+  (handler-case
+      (let ((path-parts (mapcar #'string-downcase (rest (cl-ppcre:split "/" (hunchentoot:request-uri request))))))
+        (handle-uri path-parts (slot-value acceptor 'resource)))
+    (resource-not-found-error ()
+      (error-message (setf (h:return-code*) h:+http-not-found+)))))
 
 (defun handle-uri (parts resources &optional parent)
   (let* ((keys (mapcar #'string-downcase (a:hash-table-keys resources)))
@@ -19,8 +22,7 @@
                              parent)
             (handle-collection (gethash resource-name resources)
                                parent))
-        (error-message
-         (setf (h:return-code*) h:+http-not-found+)))))
+        (error 'resource-not-found-error))))
 
 (defun handle-resource (parts resource-hash-value parent)
   (let ((resource-instance (make-instance
@@ -50,6 +52,7 @@
 (defun handle-resource-method (method resource)
   (cond ((eq method :get) (handle-get-resource resource))
         ((eq method :post) (handle-post-resource resource))
+        ((eq method :put) (handle-put-resource resource))
         ((eq method :patch) (handle-patch-resource resource))
         ((eq method :delete) (handle-delete-resource resource))
         (t (error-message
@@ -60,7 +63,19 @@
   (jonathan:to-json (view-resource resource)))
 
 (defun handle-post-resource (resource)
-  (create-resource resource (jonathan:parse (h:raw-post-data :force-text t))))
+  ;; NIY
+  )
+
+(defun handle-put-resource (resource)
+  (handler-case
+      (progn
+        (load-resource resource)
+        (let ((post-data (jonathan:parse (h:raw-post-data :force-text t))))
+          (unless (resource-equal (view-resource resource) post-data)
+            (replace-resource resource post-data))))
+    (resource-not-found-error ()
+      (create-resource resource
+                       (jonathan:parse (h:raw-post-data :force-text t))))))
 
 (defun handle-patch-resource (resource)
   (load-resource resource)
--- src/resource.lisp
+++ src/resource.lisp
@@ -1,6 +1,9 @@
 (in-package #:restful)
 
 
+(define-condition resource-not-found-error (error)
+  ())
+
 (defclass resource ()
   ((identifier :initarg :identifier)
    (parent :initarg :parent
@@ -12,6 +15,9 @@ serialized to json using the jonathan library."))
 
 (defgeneric load-resource (resource)
   (:documentation "This function loads a resource based on its identifier."))
+
+(defgeneric replace-resource (resource post-data)
+  (:documentation "This function replaces a resource."))
 
 (defgeneric create-resource (resource post-data)
   (:documentation "This function creates a new resource."))
@@ -27,8 +33,19 @@ serialized to json using the jonathan library."))
 
 (defmethod load-resource ((resource resource)))
 
+(defmethod replace-resource ((resource resource) post-data))
+
 (defmethod create-resource ((resource resource) post-data))
 
 (defmethod patch-resource ((resource resource) post-data))
 
 (defmethod delete-resource ((resource resource)))
+
+(defun resource-equal (resource1 resource2)
+  (equal (resource-normalize resource1)
+         (resource-normalize resource2)))
+
+(defun resource-normalize (resource)
+  (a:flatten (sort (a:plist-alist resource)
+                   #'(lambda (a b)
+                       (< (cdr a) (cdr b))))))
