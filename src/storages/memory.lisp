c38cc5d3d677a302218d10b11dbbbaa321040953
Florian Margaine <florian@margaine.com>
2015-05-12 21:35:59 +0200

    Adds the memory-storage methods.

--- src/storages/memory.lisp
+++ src/storages/memory.lisp
@@ -1,5 +1,17 @@
 (in-package #:restful)
 
 
+(defvar *memory-storage-items* (make-hash-table :test 'equal))
+
 (defclass memory-storage (storage)
   ())
+
+(defmethod get-item ((storage memory-storage) identifier)
+  (gethash identifier *memory-storage-items*))
+
+(defmethod delete-item ((storage memory-storage) identifier)
+  (remhash identifier *memory-storage-items*))
+
+(defmethod save-item ((storage memory-storage) resource)
+  (let ((result (view-resource resource)))
+    (setf (gethash (getf result :identifier) *memory-storage-items*) result)))
