b167ab163940c1ea31aa7ffce56b59eaf433efe8
Florian Margaine <florian@margaine.com>
2015-05-25 07:06:41 +0200

    Documents collection

--- src/collection.lisp
+++ src/collection.lisp
@@ -7,13 +7,16 @@
    (storage :initarg :storage
             :reader storage)
    (class-of-resource :initarg :class-of-resource
-                      :reader class-of-resource)))
+                      :reader class-of-resource))
+  (:documentation "Base class for restful collections. There's
+not much reason to extend it with the current features."))
 
 (defgeneric view-collection (collection)
-  (:documentation "This function should return an object that will be
+  (:documentation "Returns an object that will be
 serialized to json using the jonathan library."))
 
 (defmethod view-collection ((collection collection))
+  "Returns an object populated by the collection's storage."
   (populate-resources collection (get-items (storage collection))))
 
 (defun populate-resources (collection items)
